5086ad92020e46edf710323d4151b89806bfe887
GianniLunardi <gianni.lunardi96@gmail.com>
2022-11-22 09:34:59 +0100

    [add] of fly high exp

---
+++ include/crocoddyl/multibody/residuals/fly-high-exp.hpp
@@ -0,0 +1,138 @@
+// Author: Gianni Lunardi, University of Trento 2022
+
+#ifndef CROCODDYL_MULTIBODY_RESIDUALS_FLY_HIGH_EXP_HPP_
+#define CROCODDYL_MULTIBODY_RESIDUALS_FLY_HIGH_EXP_HPP_
+
+#include <pinocchio/multibody/fwd.hpp>
+#include <pinocchio/spatial/motion.hpp>
+#include <pinocchio/multibody/geometry.hpp>
+#include <pinocchio/algorithm/jacobian.hpp>
+#include <pinocchio/algorithm/geometry.hpp>
+#include <Eigen/src/Core/DenseBase.h>
+
+#include "crocoddyl/multibody/fwd.hpp"
+#include "crocoddyl/core/residual-base.hpp"
+#include "crocoddyl/multibody/states/multibody.hpp"
+#include "crocoddyl/multibody/data/multibody.hpp"
+#include "crocoddyl/core/utils/exception.hpp"
+
+namespace crocoddyl {
+
+/**
+
+BRIEF FOR DOXYGEN
+
+*/
+
+
+template <typename _Scalar>
+class ResidualModelFlyHighExpTpl: public ResidualModelAbstractTpl<_Scalar> {
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    typedef _Scalar Scalar;
+    typedef MathBaseTpl<Scalar> MathBase;
+    typedef ResidualModelAbstractTpl<Scalar> Base;
+    typedef ResidualDataFlyHighExpTpl<Scalar> Data;
+    typedef StateMultibodyTpl<Scalar> StateMultibody;
+    typedef ResidualDataAbstractTpl<Scalar> ResidualDataAbstract;
+    typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
+
+    typedef pinocchio::GeometryModel GeometryModel;
+    typedef typename MathBase::Vector2s Vector2s;
+    typedef typename MathBase::Vector3s Vector3s;
+    typedef typename MathBase::VectorXs VectorXs;
+    typedef typename MathBase::Matrix3s Matrix3s;
+    typedef typename MathBase::Matrix3xs Matrix3xs;
+    typedef typename MathBase::Matrix6xs Matrix6xs;
+
+    ResidualModelFlyHighExpTpl(boost::shared_ptr<StateMultibody> state,
+                               const std::size_t nu,
+                               const pinocchio::FrameIndex frame_id,
+                               const double gamma);
+
+    /*
+    Brief
+    */
+
+    virtual ~ResidualModelFlyHighExpTpl();
+
+    virtual void calc(const boost::shared_ptr<ResidualDataAbstract> &data,
+                      const Eigen::Ref<const VectorXs> &x, const Eigen::Ref<const VectorXs> &u);
+
+    virtual void calcDiff(const boost::shared_ptr<ResidualDataAbstract> &data,
+                          const Eigen::Ref<const VectorXs> &x, const Eigen::Ref<const VectorXs> &u);
+
+    virtual boost::shared_ptr<ResidualDataAbstract> createData(DataCollectorAbstract *const data);
+
+    pinocchio::FrameIndex get_frame_id() const;
+
+    Scalar get_gamma() const;
+
+  protected:
+    using Base::nu_;
+    using Base::nr_;
+    using Base::state_;
+    using Base::q_dependent_;
+    using Base::v_dependent_;
+    using Base::unone_;
+    const std::size_t nq_ = state_->get_nq();
+    const std::size_t nv_ = state_->get_nv();
+    const std::size_t ndx_ = state_->get_ndx();
+
+  private:
+    typename StateMultibody::PinocchioModel pin_model_;
+    pinocchio::FrameIndex frame_id_;
+    const double gamma_;
+
+};
+
+template <typename _Scalar>
+struct ResidualDataFlyHighExpTpl : public ResidualDataAbstractTpl<_Scalar> {
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    typedef _Scalar Scalar;
+    typedef MathBaseTpl<Scalar> MathBase;
+    typedef ResidualDataAbstractTpl<Scalar> Base;
+    typedef StateMultibodyTpl<Scalar> StateMultibody;
+    typedef DataCollectorAbstractTpl<Scalar> DataCollectorAbstract;
+
+    typedef typename MathBase::MatrixXs MatrixXs;
+    typedef typename MathBase::Matrix3xs Matrix3xs;
+    typedef typename MathBase::Matrix6xs Matrix6xs;
+    typedef typename MathBase::Vector3s Vector3s;
+    typedef typename MathBase::Vector6s Vector6s;
+    typedef typename MathBase::VectorXs VectorXs;
+
+    template <template <typename Scalar> class Model>
+    ResidualDataFlyHighExpTpl(Model <Scalar> *const model, DataCollectorAbstract *const data)
+            :   Base(model, data),
+                l_dv_dx(Matrix6xs::Zero(6, model->get_state()->get_ndx())),
+                dv_dx(Matrix3xs::Zero(3, model->get_state()->get_ndx())) {
+        // Check that proper shared data has been passed
+        DataCollectorMultibodyTpl<Scalar> *d = dynamic_cast<DataCollectorMultibodyTpl<Scalar> *>(shared);
+        if (d == NULL) {
+            throw_pretty("Invalid argument: the shared data should be derived from DataCollectorMultibodyTpl");
+        }
+
+        // Avoids data casting at runtime
+        pinocchio = d->pinocchio;
+    }
+
+    pinocchio::DataTpl<Scalar>* pinocchio;
+    Scalar h;
+    Scalar h_exp;
+    Vector6s v;
+    Matrix6xs l_dv_dx;          // derivatives in the LOCAL frame
+    Matrix3xs dv_dx;            // derivatives in the LOCAL WORLD ALIGNED frame (we need only der w.r.t. linear velocities)
+    using Base::r;
+    using Base::Ru;
+    using Base::Rx;
+    using Base::shared;
+};
+
+}
+
+#include "crocoddyl/multibody/residuals/fly-high-exp.hxx"
+
+#endif //CROCODDYL_MULTIBODY_RESIDUALS_FLY_HIGH_EXP_HPP_
---
+++ include/crocoddyl/multibody/residuals/fly-high-exp.hxx
@@ -0,0 +1,91 @@
+// Author: Gianni Lunardi, University of Trento 2022
+
+#include <pinocchio/algorithm/frames.hpp>
+#include <pinocchio/algorithm/kinematics-derivatives.hpp>
+#include <pinocchio/algorithm/frames-derivatives.hpp>
+#include "crocoddyl/core/utils/exception.hpp"
+#include "crocoddyl/core/utils/stop-watch.hpp"
+#include "crocoddyl/multibody/residuals/fly-high-exp.hpp"
+
+
+namespace crocoddyl {
+
+template <typename Scalar>
+ResidualModelFlyHighExpTpl<Scalar>::ResidualModelFlyHighExpTpl(boost::shared_ptr<StateMultibody> state,
+                                                               const std::size_t nu,
+                                                               const pinocchio::FrameIndex frame_id,
+                                                               const double gamma)
+        : Base(state, 2, nu, true, true, false),
+          pin_model_(*state->get_pinocchio()),
+          frame_id_(frame_id),
+          gamma_(gamma) {}
+
+template <typename Scalar>
+ResidualModelFlyHighExpTpl<Scalar>::~ResidualModelFlyHighExpTpl() {}
+
+template <typename Scalar>
+void ResidualModelFlyHighExpTpl<Scalar>::calc(const boost::shared_ptr<ResidualDataAbstract> &data,
+                                              const Eigen::Ref<const VectorXs> &,
+                                              const Eigen::Ref<const VectorXs> &) {
+    START_PROFILER("ResidualModelFlyHighSqr::calc");
+    Data* d = static_cast<Data*>(data.get());
+
+    pinocchio::updateFramePlacement(pin_model_, *d->pinocchio, frame_id_);
+    // Compute the velocity of the foot in the LWA reference frame
+    d->v = pinocchio::getFrameVelocity(pin_model_,
+                                       *d->pinocchio,
+                                       frame_id_,
+                                       pinocchio::LOCAL_WORLD_ALIGNED).toVector();
+
+    // Compute the height from the ground
+    d->h_exp = exp(-d->pinocchio->oMf[frame_id_].translation()[2] * gamma_);
+    // Compute the residual
+    d->r[0] = d->v[0] * d->h_exp;
+    d->r[1] = d->v[1] * d->h_exp;
+
+    STOP_PROFILER("ResidualModelFlyHighSqr::calc");
+}
+
+template <typename Scalar>
+void ResidualModelFlyHighExpTpl<Scalar>::calcDiff(const boost::shared_ptr<ResidualDataAbstract> &data,
+                                                  const Eigen::Ref<const VectorXs> &,
+                                                  const Eigen::Ref<const VectorXs> &) {
+    START_PROFILER("ResidualModelFlyHighSqr::calcDiff");
+    Data* d = static_cast<Data*>(data.get());
+    // const Eigen::IOFormat HeavyFmt(Eigen::FullPrecision, 0, ", ", ";\n", "[", "]", "[", "]");
+
+    pinocchio::getFrameVelocityDerivatives(pin_model_, *d->pinocchio, frame_id_, pinocchio::LOCAL,
+                                           d->l_dv_dx.leftCols(nv_), d->l_dv_dx.rightCols(nv_));
+    Vector3s v = pinocchio::getFrameVelocity(pin_model_, *d->pinocchio,
+                                             frame_id_, pinocchio::LOCAL).linear();
+    Matrix3s R = d->pinocchio->oMf[frame_id_].rotation();
+    // Compute the derivatives of the velocity in the LWA frame
+    Matrix3xs vxJ = pinocchio::skew(-v) * d->l_dv_dx.bottomRightCorner(3, nv_);
+    vxJ += d->l_dv_dx.topLeftCorner(3, nv_);
+
+    d->dv_dx.leftCols(nv_) = R * vxJ;
+    d->dv_dx.rightCols(nv_) = R * d->l_dv_dx.topRightCorner(3, nv_);
+    // Compute the residual Jacobian
+    d->Rx = d->h_exp * d->dv_dx.topRows(2);
+    d->Rx.leftCols(nv_) += - d->r * gamma_ * d->dv_dx.rightCols(nv_).row(2);
+    STOP_PROFILER("ResidualModelFlyHighSqr::calcDiff");
+}
+
+template <typename Scalar>
+boost::shared_ptr<ResidualDataAbstractTpl<Scalar> > ResidualModelFlyHighExpTpl<Scalar>::createData(
+        DataCollectorAbstract *const data) {
+    return boost::allocate_shared<Data>(Eigen::aligned_allocator<Data>(), this, data);
+}
+
+template <typename Scalar>
+pinocchio::FrameIndex ResidualModelFlyHighExpTpl<Scalar>::get_frame_id() const {
+    return frame_id_;
+}
+
+template <typename Scalar>
+Scalar ResidualModelFlyHighExpTpl<Scalar>::get_gamma() const {
+    return gamma_;
+}
+
+
+}   // namespace crocoddyl
